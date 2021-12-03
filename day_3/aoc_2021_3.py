bf9460e5d78e8e383709dfae11dfcdf22c422e47
BramHamaekers <bram.hamaekers@gmail.com>
2021-12-03 13:49:37 +0100

    Day 3: introduce Ternary Operators

--- day_3/aoc_2021_3.py
+++ day_3/aoc_2021_3.py
@@ -12,14 +12,12 @@ def list_to_bin(lst):
 
 
 # Get gamma value from list of numpy arrays
-def get_gamma(data):
-    matrix = np.column_stack(data)   # create rows of first bits
+def get_gamma(matrix):
     _, n = matrix.shape
     gamma = list()
     for array in matrix:
         nz = np.count_nonzero(array)
-        if nz >= n/2: gamma.append(1)
-        else: gamma.append(0)
+        gamma.append(1) if (nz >= n / 2) else gamma.append(0)
     return list_to_bin(gamma)  # convert list to binary number
 
 
@@ -29,41 +27,36 @@ def epsilon_from_gamma(gamma):
 
 
 # Calculate the power consumption given a list of numpy arrays
-def power_consumption(data):
-    gamma = get_gamma(data)
+def power_consumption(matrix):
+    gamma = get_gamma(matrix)
     epsilon = epsilon_from_gamma(gamma)
     return int(gamma, 2) * int(epsilon, 2)
 
 
 # Delete column based on bits in matrix[i]
 def delete_column(matrix, i, bit):
-    ii = np.where(matrix[i] != bit)[0]
-    jj = ii[::-1]
-    for idx in jj:
-        matrix = np.delete(matrix, idx, 1)
-
+    ii = (np.where(matrix[i] != bit)[0])[::-1]
+    for jj in ii:
+        matrix = np.delete(matrix, jj, 1)
     return matrix
 
 
 # Get o2 generator rating or co2 scrubber rating from list of numpy arrays
-def life_support_metric(data, o2=True):
-    if o2: a, b = 1, 0
-    else: a, b = 0, 1
-    matrix = np.column_stack(data)  # create rows of first bits
+def life_support_metric(matrix, o2=True):
+    a, b = (1, 0) if o2 else (0, 1)
     m, n = matrix.shape
     for i in range(m):
         nz = np.count_nonzero(matrix[i])
-        if nz >= n / 2: matrix = delete_column(matrix, i, a)
-        else: matrix = delete_column(matrix, i, b)
+        matrix = delete_column(matrix, i, a) if (nz >= n / 2) else delete_column(matrix, i, b)
         _, n = matrix.shape
         if n == 1: break
     return list_to_bin(np.transpose(matrix).flatten())
 
 
 # Calculate the life support rating given a list of numpy arrays
-def life_support(data):
-    o2 = life_support_metric(data, o2=True)
-    co2 = life_support_metric(data, o2=False)
+def life_support(matrix):
+    o2 = life_support_metric(matrix, o2=True)
+    co2 = life_support_metric(matrix, o2=False)
     return int(o2, 2) * int(co2, 2)
 
 
@@ -71,11 +64,12 @@ def main():
     # Get input data
     filename = 'input.txt'
     data = get_data(filename)
+    matrix = np.column_stack(data)  # create rows of first bits
 
     # Day 1.1
-    solution_1 = power_consumption(data)
+    solution_1 = power_consumption(matrix)
     # Day 1.2
-    solution_2 = life_support(data)
+    solution_2 = life_support(matrix)
     # Report solution
     print(f'solution 1: {solution_1}\nsolution 2: {solution_2}')
 
